79dc482c62c6d22cf1b394204d073af31bbf053d
Ariadna <achicol1900@alumno.ipn.mx>
2021-05-08 20:36:49 -0500

    validación de espacios vacíos

--- js/crearCuenta.js
+++ js/crearCuenta.js
@@ -1,1 +1,19 @@
-// Escribe algo
+// JS crear cuenta
+
+function validar(){
+    vacio();
+}
+
+// Se completan todos los espacios
+function vacio(){
+    if(document.querySelector('#nombre').value.trim()=="" || document.querySelector('#correo').value.trim()=="" ||
+        document.querySelector('#cont').value.trim()=="" || document.querySelector('#edad').value.trim()==""){
+        console.log('hay espacios vacíos en crear cuenta');
+        alert('Por favor, complete todos los espacios');
+        event.preventDefault()
+    }else{
+            console.log('todos los espacios contienen caracteres');
+            event.preventDefault();
+        }
+}
+
